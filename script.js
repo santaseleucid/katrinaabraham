60c486076b3366b83d7dcb9610e8696fc0d45036
Sanjay Abraham <sabraham@korewireless.com>
2024-07-05 16:03:29 -0500

    working

--- script.js
+++ script.js
@@ -22,42 +22,42 @@ window.addEventListener("load", function () {
     performance_data.responseEnd - performance_data.requestStart;
   let render_time = performance_data.domComplete - performance_data.domLoading;
 
-  console.log("Screen Width:", screen_width);
-  console.log("Screen Height:", screen_height);
-  console.log("Screen Available Width:", screen_available_width);
-  console.log("Screen Available Height:", screen_available_height);
-  console.log("Browser Type:", browser_type);
-  console.log("Language:", language);
-  console.log("Time Zone Offset:", time_zone_offset);
-  console.log("Page Load Time:", page_load_time);
-  console.log("Request Response Time:", request_response_time);
-  console.log("Render Time:", render_time);
+  // console.log("Screen Width:", screen_width);
+  // console.log("Screen Height:", screen_height);
+  // console.log("Screen Available Width:", screen_available_width);
+  // console.log("Screen Available Height:", screen_available_height);
+  // console.log("Browser Type:", browser_type);
+  // console.log("Language:", language);
+  // console.log("Time Zone Offset:", time_zone_offset);
+  // console.log("Page Load Time:", page_load_time);
+  // console.log("Request Response Time:", request_response_time);
+  // console.log("Render Time:", render_time);
 
   // page load time: wait until load event is finished with setTimeout
   setTimeout(function () {
-    //var form = document.getElementById('data-form');
-    //var formData = new FormData(form);
-   
-    
-    var xhr = new XMLHttpRequest();
-    var url = "https://script.google.com/macros/s/AKfycbyjS_Zwyy9vbUANmePxzjSxdGRg79IgNma_rwz6DmsVqbxucVR_QeAaI-XTVe96bDlcwA/exec"; // Replace with your Web App URL
-            //"https://script.google.com/macros/s/AKfycbyjS_Zwyy9vbUANmePxzjSxdGRg79IgNma_rwz6DmsVqbxucVR_QeAaI-XTVe96bDlcwA/exec"
-            //"https://script.google.com/macros/s/AKfycbyjS_Zwyy9vbUANmePxzjSxdGRg79IgNma_rwz6DmsVqbxucVR_QeAaI-XTVe96bDlcwA/exec"
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+
+    var url = "https://script.google.com/macros/s/AKfycbztymiZjI8iTeWYt-_wYeZn8gcqcOq2nEFy26V9DDmGl5RoRAn_A2WETze11jJ7FR6UOQ/exec"; // Replace with your Web App URL
 
     var jsonData = {
-        "field1": "John Doe",
-        "field2": "johndoe@example.com"
+      screen_width: screen_width,
+      screen_height: screen_height,
+      screen_available_width: screen_available_width,
+      screen_available_height: screen_available_height,
+      browser_type: browser_type,
+      language: language,
+      time_zone_offset: time_zone_offset,
+      page_load_time: page_load_time,
+      request_response_time: request_response_time,
+      render_time: render_time
     };
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            alert("JSON data submitted successfully!");
-        }
-    };
-
-    xhr.send(JSON.stringify(jsonData));
+    fetch(url, {
+      method: 'POST',
+      // headers: {
+      //   'Content-Type': 'application/json'
+      // },
+      body: JSON.stringify(jsonData),
+      redirect: 'follow'
+    })
   }, 0);
 });
 
